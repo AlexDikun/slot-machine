a30d90d750dad7b16f4507b9c3f8d12007d6ea79
valfeo16 <valfeo16@yandex.ru>
2026-01-03 00:36:32 +0300

    добавил компоненты: шапка, мейн (и вложенный в него слот-машина) и подвал. Добавил простые стили, растянул компоненты на весь экран, подвал прижал к низу экрана

---
+++ src/app/main/slot-machine/slot-machine.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-slot-machine',
+  standalone: false,
+  templateUrl: './slot-machine.component.html',
+  styleUrl: './slot-machine.component.scss'
+})
+export class SlotMachineComponent {
+
+}
