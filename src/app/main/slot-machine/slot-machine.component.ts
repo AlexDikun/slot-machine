37c09a01c348f3ad6e0d099ab7a045483cfaaac5
valfeo16 <valfeo16@yandex.ru>
2026-01-04 23:23:37 +0300

    теперь есть барабан 5*3 с иконками персонажей из трех богатырей. Кнопка Спин просто генерирует новый порядок отображения иконок. Вся логика прописана в компоненте слот-машины (это временно)

--- src/app/main/slot-machine/slot-machine.component.ts
+++ src/app/main/slot-machine/slot-machine.component.ts
@@ -1,5 +1,11 @@
 import { Component } from '@angular/core';
 
+interface Symbol {
+  id: string;
+  name: string;
+  icon: string;
+}
+
 @Component({
   selector: 'app-slot-machine',
   standalone: false,
@@ -7,12 +13,25 @@ import { Component } from '@angular/core';
   styleUrls: ['./slot-machine.component.scss']
 })
 export class SlotMachineComponent {
-  symbols = ['🍒', '🍋', '🔥', '💎', '7', '🍀'];
-  reels = [
-    ['🍒', '🍋', '🔥'],
-    ['💎', '7', '🍀'],
-    ['🍒', '🍋', '🔥']
+  symbols: Symbol[] = [
+    { id: 'six', name: 'Crow', icon: 'assets/symbols/six-icon.jpg'},
+    { id: 'seven', name: 'Basileus', icon: 'assets/symbols/seven-icon.jpg'},
+    { id: 'eight', name: 'Solovey', icon: 'assets/symbols/eight-icon.jpg'},
+    { id: 'nine', name: 'Felicita', icon: 'assets/symbols/nine-icon.jpg'},
+    { id: 'ten', name: 'Tugarin', icon: 'assets/symbols/ten-icon.jpg'},
+    { id: 'jack', name: 'Kalyvan', icon: 'assets/symbols/jack-icon.jpg'},
+    { id: 'lady', name: 'BabaYaga', icon: 'assets/symbols/lady-icon.jpg'},
+    { id: 'king', name: 'Koschei', icon: 'assets/symbols/king-icon.jpg'},
+    { id: 'ace', name: 'Gorynych', icon: 'assets/symbols/ace-icon.jpg'},
+    { id: 'wild1', name: 'Gold', icon: 'assets/symbols/wildGoldenHorseshoe-icon.jpg'},
+    { id: 'wild2', name: 'Silver', icon: 'assets/symbols/wildSilverHorseshoe-icon.jpg'},
   ];
+  reels: Symbol[][] = [
+    Array(5).fill(null).map(() => this.symbols[Math.floor(Math.random() * this.symbols.length)]),
+    Array(5).fill(null).map(() => this.symbols[Math.floor(Math.random() * this.symbols.length)]),
+    Array(5).fill(null).map(() => this.symbols[Math.floor(Math.random() * this.symbols.length)]),
+  ];
+
   spin() {
     // Перемешиваем символы
     this.reels = this.reels.map(reel =>
