199d0cbddb7a1e059fd584be46779102bad971e0
AlexDikun <loban.sasha.97@mail.ru>
2026-01-14 20:23:39 +0300

    добавил единый конфиг для компонента, в который внес константЫ: кол-во барабанов, количетсво видимых висмолов в барабане, высоту одного символа и значение длительности одного спина

--- src/app/games/slot-machine/config/index.ts
+++ src/app/games/slot-machine/config/index.ts
@@ -1,3 +1,4 @@
 // src/app/games/slot-machine/config/index.ts
 
 export * from './symbols.config';
+export * from './slot-machine.config';
---
+++ src/app/games/slot-machine/config/slot-machine.config.ts
@@ -0,0 +1,8 @@
+// src/app/games/slot-machine/config/slot-machine.config.ts
+
+export const SLOT_MACHINE_CONFIG = {
+  REELS_COUNT: 5,
+  VISIBLE_SYMBOLS: 3,
+  SYMBOL_HEIGHT: 60,
+  SPIN_BASE_DURATION: 2.5,
+} as const;
