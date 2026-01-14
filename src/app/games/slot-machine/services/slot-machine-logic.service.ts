66901b0b94319bec932901d8ca75a16a44dd3839
AlexDikun <loban.sasha.97@mail.ru>
2026-01-15 00:42:00 +0300

    переписана функция calculateWin - теперь она принимает в параметр reels не string, a SymbolConfig[][] и система расчета переделана, добавлен типобезопасный расчет результата ставки - const multiplier = symbol.multipliers?.[count] ?? 1; То есть, если выигрыша нет, то просто multiplier присваивает единицу

--- src/app/games/slot-machine/services/slot-machine-logic.service.ts
+++ src/app/games/slot-machine/services/slot-machine-logic.service.ts
@@ -9,31 +9,26 @@ import { SymbolConfig } from '../config/symbols.config';
 export class SlotMachineLogicService {
   constructor() {}
 
-  generateReels(): string[][] {
-    return Array.from({ length: 5 }, () => 
+  private getRandomSymbol(): SymbolConfig {
+    return SYMBOLS_CONFIG[Math.floor(Math.random() * SYMBOLS_CONFIG.length)];
+  }
+
+  generateReels(): SymbolConfig[][] {
+    return Array.from({ length: 5 }, () =>
       Array.from({ length: 3 }, () => this.getRandomSymbol())
     );
   }
 
-  private getRandomSymbol(): string {
-    const symbol = SYMBOLS_CONFIG[Math.floor(Math.random() * SYMBOLS_CONFIG.length)];
-    return symbol.id;
-  }
-
-  calculateWin(reels: string[][], bet: number): number {
+  calculateWin(reels: SymbolConfig[][], bet: number): number {
     let win = 0;
-    
-    for (let i = 0; i < 5; i++) {
-      const column = reels[i];
-      const symbol = column[0]; // Проверка первого символа в колонке
 
-      // Если все символы на барабанах совпадают
-      if (column.every(s => s === symbol)) {
-        const symbolConfig = SYMBOLS_CONFIG.find(s => s.id === symbol);
-        if (symbolConfig) {
-          const multiplier = symbolConfig?.multipliers?.[3] || 1; 
-          win += bet * multiplier;
-        }
+    for (let i = 0; i < reels.length; i++) {
+      const column = reels[i];
+      const symbol = column[0];
+      const count = column.length as 3 | 4 | 5;
+      if (column.every(s => s.id === symbol.id)) {
+        const multiplier = symbol.multipliers?.[count] ?? 1;
+        win += bet * multiplier;
       }
     }
 
